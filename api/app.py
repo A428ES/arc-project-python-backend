2a29424ac70c2bfabb617f2ca360bd4a16b24813
Sergio <estrada.serg@gmail.com>
2023-03-24 15:18:44 -0500

    registered new blueprint routes

--- api/app.py
+++ api/app.py
@@ -4,7 +4,13 @@ def create_app(config=None):
     app = Flask(__name__)
 
     from api.user.user import user_route
+    from api.admin.admin import admin_route
+    from api.stories.stories import stories_route
+    from api.comments.comments import comment_route
 
     app.register_blueprint(user_route)
+    app.register_blueprint(admin_route)
+    app.register_blueprint(stories_route)
+    app.register_blueprint(comment_route)
 
     return app
