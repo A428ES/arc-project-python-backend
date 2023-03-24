0cc55822a4590a3339a2ba6160bc8ab6c12d55c1
Sergio <estrada.serg@gmail.com>
2023-03-24 16:01:37 -0500

    added template schema comment

--- db/mongo_schemas.py
+++ db/mongo_schemas.py
@@ -1,9 +1,37 @@
 class MongoSchemas:
     def new_user_account():
+        #uuid
+        #first name
+        #last name
+        #email
+        #password
+        #preferences object placeholder
+
+        ####universal
+        #ip address
+        #timestamp
+        #deleted
         pass
 
     def new_comment():
+        #comment html/body
+        #author uuid
+        #ip address
+
+        ####universal
+        #ip address
+        #timestamp
+        #deleted
         pass
 
     def new_story():
+        #title
+        #body / html
+        #author uuid
+        #ip address
+
+        ####universal
+        #ip address
+        #timestamp
+        #deleted
         pass
