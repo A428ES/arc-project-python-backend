d55a4d93f1faefbaa08785e83092feed5090c9ac
Sergio <estrada.serg@gmail.com>
2023-03-24 20:20:33 -0500

    added schema validation function

--- db/mongo_schemas.py
+++ db/mongo_schemas.py
@@ -1,21 +1,28 @@
 class MongoSchemas:
-    def new_user_account(first_name, last_name, email, password):
+    def schema_builder(self, value, val_type="string", length=20, regex="standard"):
+        return {'value':value,
+                'type':val_type,
+                'length':length,
+                'regex':None
+        }
+    
+    def new_user_account(self, first_name, last_name, email, password):
         return {
-            'first_name': first_name,
-            'last_name': last_name,
-            'email': email,
-            'password': password
+            'first_name': self.schema_builder(first_name),
+            'last_name': self.schema_builder(last_name),
+            'email': self.schema_builder(email, length=30, regex="email"),
+            'password': self.schema_builder(password, lenght=16, regex="password"),
         }
 
-    def new_comment(comment, author_uuid):
+    def new_comment(self, comment, author_uuid):
         return {
-            'comment': comment,
-            'author_uuid': author_uuid,
+            'comment': self.schema_builder(comment, length=500),
+            'author_uuid': self.schema_builder(author_uuid, regex="uuid"),
         }
 
-    def new_story(title, story, author_uuid):
+    def new_story(self, title, story, author_uuid):
         return {
-            'title': title,
-            'story': story,
-            'author_uuid': author_uuid,
+            'title': self.schema_builder(title, length=30),
+            'story': self.schema_builder(story, length=None),
+            'author_uuid': self.schema_builder(author_uuid, regex="uuid"),
         }
