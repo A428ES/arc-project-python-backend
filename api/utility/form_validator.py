396648655ed30acdab50de1e47d15ed677a198b0
Sergio <estrada.serg@gmail.com>
2023-03-25 16:42:37 -0500

    added final validation functions

--- api/utility/form_validator.py
+++ api/utility/form_validator.py
@@ -1,3 +1,7 @@
+from config.config import Config
+import re
+import html
+
 class FormValidator:
     def __init__(self, current_user, json_request, schema_object):
         self.json_req = json_request
@@ -6,21 +10,17 @@ class FormValidator:
         self.json_req["author_uuid"] = current_user["uuid"]
 
         self.minimum_key_match()
-        self.validate_form()
 
     def field_validate_length(self, entry):
-        if (
-            self.schema[entry]["length"] is None
-            or self.schema[entry]["type"] != "string"
-        ):
+        if self.schema[entry]["length"] is None:
             return True
 
         if len(self.json_req[entry]) > self.schema[entry]["length"]:
-            raise Exception("Entry exceeds length")
+            raise Exception(f"{entry} exceeds length")
 
     def field_validate_not_empty(self, entry):
         if self.json_req[entry] in ["", None, "None"]:
-            raise Exception("This field cannot be left empty")
+            raise Exception(f"{entry}  cannot be left empty")
 
     def field_validate_type(self, entry):
         try:
@@ -29,7 +29,7 @@ class FormValidator:
             elif self.schema[entry]["type"] == "int":
                 self.json_req[entry] = int(self.json_req[entry])
         except Exception:
-            raise Exception("Invalid type for field")
+            raise Exception(f"{entry} Invalid type for field")
 
     def minimum_key_match(self):
         key_match = [key for key in self.schema.keys() if key in self.json_req.keys()]
@@ -37,18 +37,31 @@ class FormValidator:
         if len(key_match) != len(self.schema.keys()):
             raise Exception("The request object is missing required keys")
 
-    def regex_validation(self):
-        pass
+    def regex_validation(self, entry):
+        if self.schema[entry]['regex'] == None:
+            return True
+        
+        if re.match(Config.regex_dict[self.schema[entry]['regex']], self.json_req[entry]) is None:
+            raise Exception(f"{entry} Does not match required regex")
+        
+    def final_sanitiation(self, entry):
+        if not isinstance(self.json_req[entry], str): 
+            return self.json_req[entry]
+        
+        return html.escape("".join([char for char in self.json_req[entry] if char not in Config.remove_characters]))
 
     def validate_request_field(self, entry):
         self.field_validate_type(entry)
-        self.field_validate_length(entry)
-        self.field_validate_not_empty(entry)
 
-        self.schema[entry]["value"] = self.json_req[entry]
+        if self.schema[entry]['type'] == "string":
+            self.field_validate_length(entry)
+            self.field_validate_not_empty(entry)
+            self.regex_validation(entry)
+
+        self.json_req[entry] = self.final_sanitiation(entry)
 
     def validate_form(self):
         for item in self.json_req.keys():
             self.validate_request_field(item)
 
-        return self.schema
+        return self.json_req
