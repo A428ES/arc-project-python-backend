0946ebbcdd9197bc8fb3e99d6644a7cc1d4a8b56
Sergio <estrada.serg@gmail.com>
2023-03-25 16:42:49 -0500

    updated mongo schemas

--- db/mongo_schemas.py
+++ db/mongo_schemas.py
@@ -1,26 +1,26 @@
 class MongoSchemas:
     def schema_builder(
-        self, value=None, val_type="string", length=20, regex="standard"
+        self, value=None, val_type="string", length=36, regex=None
     ):
-        return {"value": value, "type": val_type, "length": length, "regex": None}
+        return {"value": value, "type": val_type, "length": length, "regex": regex}
 
-    def new_user_account(self, first_name, last_name, email, password):
+    def new_user_account(self):
         return {
-            "first_name": self.schema_builder(first_name),
-            "last_name": self.schema_builder(last_name),
-            "email": self.schema_builder(email, length=30, regex="email"),
-            "password": self.schema_builder(password, lenght=16, regex="password"),
+            "first_name": self.schema_builder(regex="name"),
+            "last_name": self.schema_builder(regex="name"),
+            "email": self.schema_builder(length=30, regex="email"),
+            "password": self.schema_builder(lenght=16, regex="password"),
         }
 
     def new_comment(self):
         return {
-            "comment": self.schema_builder(length=500, val_type="int"),
+            "comment": self.schema_builder(length=500),
             "author_uuid": self.schema_builder(regex="uuid"),
         }
 
-    def new_story(self, title, story, author_uuid):
+    def new_story(self):
         return {
-            "title": self.schema_builder(title, length=30),
-            "story": self.schema_builder(story, length=None),
-            "author_uuid": self.schema_builder(author_uuid, regex="uuid"),
+            "title": self.schema_builder(length=30),
+            "story": self.schema_builder(length=None),
+            "author_uuid": self.schema_builder(regex="uuid"),
         }
