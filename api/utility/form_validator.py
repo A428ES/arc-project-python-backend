d3099136dbf2f4a1516ca94bc1b270a172036ff3
Sergio <estrada.serg@gmail.com>
2023-03-24 20:20:39 -0500

    created form validator class

---
+++ api/utility/form_validator.py
@@ -0,0 +1,9 @@
+class FormValidator:
+    def __init__(self, json_request, schema_object):
+        pass
+    
+    def validate_request_form():
+        pass
+
+    def insert_uuid(self):
+        pass
