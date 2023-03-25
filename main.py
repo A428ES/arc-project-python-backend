14aae446784250be78511fad0ba66291673ca59b
Sergio <estrada.serg@gmail.com>
2023-03-25 09:58:57 -0500

    testing revisions

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-from api.app import create_app
+# from api.app import create_app
 
-run_app = create_app()
-run_app.run()
+# run_app = create_app()
+# run_app.run()
+
+from db.mongo_schemas import MongoSchemas
+from api.utility.form_validator import FormValidator
+
+new_schema = MongoSchemas().new_comment()
+test = {'comment':99}
+current_user = {'uuid':"current_user"}
+new_validation = FormValidator(current_user, test, new_schema)
+
+print(new_validation.schema)
