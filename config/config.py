792309a3f49d22a099083c631b3c9831c1d472d5
Sergio <estrada.serg@gmail.com>
2023-03-25 16:42:43 -0500

    added regex config

--- config/config.py
+++ config/config.py
@@ -1,2 +1,11 @@
 class Config:
     test_config = None
+    
+    regex_dict = {
+        'name':"^[a-z]*$",
+        'email': r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b",
+        'password:':'[A-Za-z0-9@#$]{16}',
+        'uuid':'[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}'
+    }
+
+    remove_characters = ['>', '<', '"', "'", ':', ';', '*']
