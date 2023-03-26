327b2c919be6a841e267a0f9f818a907b60f26ae
Sergio <estrada.serg@gmail.com>
2023-03-26 01:43:55 -0500

    added regex dict

--- config/config.py
+++ config/config.py
@@ -2,9 +2,9 @@ class Config:
     test_config = None
 
     regex_dict = {
-        "name": "^[a-z]*$",
+        "name": "[a-zA-Z]{2,}",
+        "password": "[A-Za-z0-9@#$]{8,16}",
         "email": r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b",
-        "password:": "[A-Za-z0-9@#$]{16}",
         "uuid": "[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}",
     }
 
