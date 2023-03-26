8dd2b3a3a8b2a3f78bdbb5301111042d789ff9d3
Sergio <estrada.serg@gmail.com>
2023-03-26 09:38:54 -0500

    updated user class implementation

--- api/utility/user_login.py
+++ api/utility/user_login.py
@@ -2,13 +2,18 @@ from flask_login import UserMixin
 from db.mongo_controller import MongoController
 
 class User(UserMixin, MongoController):
-    def __init__(self, user_id):
-        self.find(user_id)
+    def __init__(self):
+        pass
 
     def find(self, user_id):
         self.db = MongoController()
 
         user = self.db.find_record("users", {'email':user_id})
 
+        if user == None:
+            return None
+
         self.id = user['email']
         self.password = user['password']
+
+        return self
