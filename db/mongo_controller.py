937613959355ace5f7fbe166ebe53eb81ae134b2
Sergio <estrada.serg@gmail.com>
2023-03-26 01:44:03 -0500

    fixed bug with find record function

--- db/mongo_controller.py
+++ db/mongo_controller.py
@@ -14,7 +14,7 @@ class MongoController(MongoSchemas):
         self.collection = self.db[collection]
 
     def inject_tracking_requirements(self, db_entry):
-        db_entry["uuid"] = (str(uuid.uuid4()),)
+        db_entry["uuid"] = str(uuid.uuid4())
         db_entry["ip_address"] = "127.0.0.1"
         db_entry["timestamp_ms"] = datetime.now().timestamp()
         db_entry["is_deleted"] = False
