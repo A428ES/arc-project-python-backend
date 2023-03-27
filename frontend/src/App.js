76770014b98fc76e19071b5d452ae42f1f9752ef
Sergio <estrada.serg@gmail.com>
2023-03-26 21:18:28 -0500

    removed redundant import

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
 import React, { useEffect, useState } from "react";
-import "../src/user/login";
 import UserLogin from "../src/user/login";
 import UserLoggedIn from "../src/user/loggedin";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
