7c471fee7b8d7abeb482f73a6d1461bd53c80698
Sergio <estrada.serg@gmail.com>
2023-04-02 18:11:11 -0500

    added functionality to load logged in user

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,45 @@
 import "./App.css";
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import NavigationBar from "./layout/navigationbar";
+import Content from "./layout/content";
+import UserLogin from "./user/login";
+import { AuthContext } from "./context/user_context";
 
 function App() {
+  const [authState, setAuthState] = useContext(AuthContext);
+
+  useEffect(() => {
+    const loggedInUser = localStorage.getItem("user_token");
+
+    if (loggedInUser) {
+      let lookUpUser = () => {
+        fetch("http://localhost:5000//user/check_logged_in", {
+          headers: { Authorization: "Bearer " + loggedInUser },
+          method: "GET",
+        })
+          .then((response) => {
+            if (response.ok) {
+              return response.json();
+            }
+          })
+          .then((data) => {
+            setAuthState({ userLoggedIn: true, userData: data.results });
+          });
+      };
+
+      lookUpUser();
+    }
+  }, []);
+
   return (
     <>
       <NavigationBar />
       <Router>
-        <Routes></Routes>
+        <Routes>
+          <Route path="/" element={<Content />} />
+          <Route path="/login" element={<UserLogin />} />
+        </Routes>
       </Router>
     </>
   );
