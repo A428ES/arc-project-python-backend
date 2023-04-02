81fc237f059c4c2ff4dc3ae5387676e954c77d33
Sergio <estrada.serg@gmail.com>
2023-04-02 16:35:36 -0500

    added error handling to user route

--- api/user/user.py
+++ api/user/user.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
 from flask_jwt_extended import jwt_required, create_access_token
 from api.utility.user_login import User
 from api.app import db
@@ -6,42 +6,62 @@ from api.utility.form_validator import FormValidator
 
 user_route = Blueprint("user", __name__)
 
+
+@user_route.errorhandler(Exception)
+def handle_general_exception(e):
+    return {"error": str(e)}, 400
+
+
 @user_route.route("/user", methods=["GET"])
 def main_user():
     return {"results": "main user"}
 
-@user_route.route("/user/register", methods=['GET'])
+
+@user_route.route("/user/register", methods=["GET"])
 def user_register():
-    request_args = {"first_name": request.args.get("firstname"),
-                    "last_name":request.args.get("lastname"), 
-                    "email":request.args.get("email"),
-                    "password":request.args.get("password")}
-    
+    request_args = {
+        "first_name": request.args.get("firstname"),
+        "last_name": request.args.get("lastname"),
+        "email": request.args.get("email"),
+        "password": request.args.get("password"),
+    }
+
     new_validation = FormValidator(request_args, db.new_user_account()).validate_form()
-    
+
     return db.insert_record("users", new_validation)
 
-@user_route.route("/user/login", methods=['GET'])
+
+@user_route.route("/user/login", methods=["GET"])
 def user_login_view():
-    request_args = {"email":request.args.get("email"),
-                    "password":request.args.get("password")}
-    
+    print("test")
+    print("test")
+    print("test")
+    print("test")
+    request_args = {
+        "email": request.args.get("email"),
+        "password": request.args.get("password"),
+    }
+
     processed_request = FormValidator(request_args, db.new_login()).validate_form()
-    locate_user = db.find_record('users', {'email':processed_request['email']})
 
+    locate_user = db.find_record("users", {"email": processed_request["email"]})
+    print("test")
+    print(locate_user)
     if locate_user != None:
-        if processed_request['password'] == locate_user['password']:
-            access_token = create_access_token(identity=locate_user['email'])
+        if processed_request["password"] == locate_user["password"]:
+            access_token = create_access_token(identity=locate_user["email"])
 
-            return {'results':access_token}
+            return {"results": access_token}
 
-    return {'results':'invalid login attempt'}
+    raise Exception("invalid login attempt")
 
-@user_route.route("/user/logout", methods=['GET'])
+
+@user_route.route("/user/logout", methods=["GET"])
 def user_logout():
-    return {'results':'logged out'}
+    return {"results": "logged out"}
 
-@user_route.route("/user/check_logged_in", methods=['GET'])
+
+@user_route.route("/user/check_logged_in", methods=["GET"])
 @jwt_required()
 def check_logged_in():
-    return {'results':'logged in'}
+    return {"results": "logged in"}
