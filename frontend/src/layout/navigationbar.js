ad90e0072d28c4015e7108f63249b0b84960afa0
Sergio <estrada.serg@gmail.com>
2023-04-02 18:11:42 -0500

    updated nav bar to new auth protocols

--- frontend/src/layout/navigationbar.js
+++ frontend/src/layout/navigationbar.js
@@ -1,22 +1,21 @@
 import React from "react";
 import { AuthContext } from "../context/user_context";
 import { useContext } from "react";
-import UserLogin from "../user/login";
 
 export default function NavigationBar() {
   const [authState, setAuthState] = useContext(AuthContext);
 
   const handleLogout = () => {
-    authState.userLoggedIn = false;
-    setAuthState(authState);
+    setAuthState({ userLoggedIn: false });
+    localStorage.removeItem("user_token");
   };
 
   return (
     <>
       {authState.userLoggedIn === true ? (
         <div class="topnav">
-          <a class="user">Welcome, {authState.username}</a>
-          <a href="#main">Main Page</a>
+          <a class="user">Welcome, {authState.userData}</a>
+          <a href="/">Main Page</a>
           <a href="#submit">Submit Story</a>
           <a href="#mysubmissions">My Submissions</a>
           <a href="#mycomments">My Comments</a>
@@ -26,13 +25,13 @@ export default function NavigationBar() {
           </a>
         </div>
       ) : (
-        <div class="topnav">
-          <a class="user">
-            <UserLogin />
-          </a>
-          <a href="#main">Main Page</a>
-          <a href="#main">Register</a>
-        </div>
+        <>
+          <div class="topnav">
+            <a href="#main">Main Page</a>
+            <a href="#main">Register</a>
+            <a href="/login">Login</a>
+          </div>
+        </>
       )}
     </>
   );
