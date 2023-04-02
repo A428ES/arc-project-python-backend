2eec79414cc8790dc1d3bc55f6dc81ec57f55cfa
Sergio <estrada.serg@gmail.com>
2023-04-02 18:11:23 -0500

    removed react strict enforcement

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,9 +7,7 @@ import { AuthContextProvider } from "../src/context/user_context";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <React.StrictMode>
-    <AuthContextProvider>
-      <App />
-    </AuthContextProvider>
-  </React.StrictMode>
+  <AuthContextProvider>
+    <App />
+  </AuthContextProvider>
 );
