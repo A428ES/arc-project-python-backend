52513ac0f7c28e31edae22c2033f8d3a3d22ff1a
Sergio <estrada.serg@gmail.com>
2023-04-03 14:31:01 -0500

    added registration form

--- frontend/src/pages/user/register.js
+++ frontend/src/pages/user/register.js
@@ -6,7 +6,38 @@ export default function RegisterAccount() {
       <header class="articleHeader" id="p1">
         Register New Account
       </header>
-      <p>Registration form goes here</p>
+      <p>
+        <div class="content">
+          <div class="loginError"></div>
+          <form>
+            <label>
+              First Name: <input type="text" name="firstname" />
+            </label>
+
+            <label>
+              Last Name: <input type="text" name="lastname" />
+            </label>
+            <br />
+            <label>
+              Email: <input type="email" name="email" />
+            </label>
+
+            <label>
+              Confirm Email: <input type="email" name="email" />
+            </label>
+            <br />
+            <label>
+              Password: <input type="password" name="password" />
+            </label>
+
+            <label>
+              Confirm Password:{" "}
+              <input type="password" name="confirm_password" />
+            </label>
+            <input value="Login" type="submit" />
+          </form>
+        </div>
+      </p>
     </>
   );
 }
