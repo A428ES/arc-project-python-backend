c36617008c292366ca697b39fffb0541cf879e53
Sergio <estrada.serg@gmail.com>
2023-04-08 09:36:02 -0500

    removed stale import

--- frontend/src/components/comment_bar.js
+++ frontend/src/components/comment_bar.js
@@ -1,4 +1,4 @@
-import { useEffect, useState, useContext } from "react";
+import { useEffect, useState } from "react";
 import CommentsOnStory from "../pages/user/storycomments";
 import HTTPRequester from "../utility/requester";
 
