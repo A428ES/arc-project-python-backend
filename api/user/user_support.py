f2b207700045fae2fb5e512ef23f2b30e89585e2
FIRST_NAME LAST_NAME <estrada.serg@gmail.com>
2023-08-02 09:24:55 -0500

    fixed bug with workign user drill in

--- api/user/user_support.py
+++ api/user/user_support.py
@@ -1,15 +1,15 @@
 class UserSupport:
-    def __init__(working_user):
+    def __init__(self, db, working_user):
         self.db = db
         self.working_user = working_user
 
-    def get_user_data():
+    def get_user_data(self):
         return {"results": {
-            "email": current_user["email"],
-            "firstname": current_user["first_name"],
-            "lastname": current_user["last_name"],
-            "created": current_user["created_timestamp_ms"],
-            "uuid": current_user["uuid"],
-            "story_count": len(db.find_record("stories", {"author_uuid":self.working_user['uuid']}, first=False)),
-            "comment_count": len(db.find_record("comments", {"author_uuid":self.working_user['uuid']}, first=False))
+            "email": self.working_user["email"],
+            "firstname": self.working_user["first_name"],
+            "lastname": self.working_user["last_name"],
+            "created": self.working_user["created_timestamp_ms"],
+            "uuid": self.working_user["uuid"],
+            "story_count": len(self.db.find_record("stories", {"author_uuid":self.working_user['uuid']}, first=False)),
+            "comment_count": len(self.db.find_record("comments", {"author_uuid":self.working_user['uuid']}, first=False))
         }}
