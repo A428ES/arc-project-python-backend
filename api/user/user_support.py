bb046270a2ad746c0bb8be7e1fd753d8d7023b92
FIRST_NAME LAST_NAME <estrada.serg@gmail.com>
2023-08-05 11:46:20 -0500

    added date formatting to created attribute

--- api/user/user_support.py
+++ api/user/user_support.py
@@ -6,11 +6,13 @@ class UserSupport:
         self.working_user = working_user
 
     def get_user_data(self):
+        dt_from_timestamp = datetime.fromtimestamp(self.working_user["created_timestamp_ms"])
+
         return {"results": {
             "email": self.working_user["email"],
             "firstname": self.working_user["first_name"],
             "lastname": self.working_user["last_name"],
-            "created": self.working_user["created_timestamp_ms"],
+            "created": datetime.strftime(dt_from_timestamp, "%m/%d/%Y"),
             "uuid": self.working_user["uuid"],
             "story_count": len(self.db.find_record("stories", {"author_uuid":self.working_user['uuid']}, first=False)),
             "comment_count": len(self.db.find_record("comments", {"author_uuid":self.working_user['uuid']}, first=False))
