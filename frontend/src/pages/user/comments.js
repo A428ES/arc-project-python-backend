b613d148884a71d2d0f8f1dbc09776babfb9ea51
FIRST_NAME LAST_NAME <estrada.serg@gmail.com>
2023-12-02 18:12:12 -0600

    fixed bug where deletion of comments would appear not to work

--- frontend/src/pages/user/comments.js
+++ frontend/src/pages/user/comments.js
@@ -1,19 +1,20 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import CommentDisplay from "../../components/comment_display";
 import { AuthContext } from "../../context/user_context";
 import PageTitle from "../../components/page_title";
+import HTTPRequester from "../../utility/requester";
 
 export default function MyComments() {
-  const [authState, setAuthState] = useContext(AuthContext);
-
+  const [newComment, setNewComment] = useState();
+  
   return (
     <>
       <PageTitle text="Viewing Your Comments" />
       <CommentDisplay
         viewType="comments/mycomments"
         storyID={null}
-        setNew={() => {}}
-        newComment={null}
+        setNew={setNewComment}
+        newComment={newComment}
       />
     </>
   );
